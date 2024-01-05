7f277310e55bb6fcfca24f46a01f762e23f16f62
yamashush <38120991+yamashush@users.noreply.github.com>
2024-01-05 21:18:44 +0900

    bundle gem deep_compactor

---
+++ lib/deep_compactor.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "deep_compactor/version"
+
+module DeepCompactor
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/deep_compactor_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe DeepCompactor do
+  it "has a version number" do
+    expect(DeepCompactor::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
