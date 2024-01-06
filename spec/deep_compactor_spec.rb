bb47c609e6dba1859e92d6eb3381b95247dffcd0
yamashush <38120991+yamashush@users.noreply.github.com>
2024-01-06 16:18:47 +0900

    Add deep_compact_blank tests

--- spec/deep_compactor_spec.rb
+++ spec/deep_compactor_spec.rb
@@ -14,8 +14,24 @@ RSpec.describe DeepCompactor do
     using RSpec::Parameterized::TableSyntax
 
     where(:source, :compacted) do
-      ["a", nil, [], ["aa", "bb", nil]] | ["a", [], %w[aa bb]]
-      ["a", "b", [], %w[aa bb cc]]      | ref(:source)
+      ["a", nil, [], ["aa", "bb", nil, []]] | ["a", [], ["aa", "bb", []]]
+      ["a", "b", [], %w[aa bb cc dd]]       | ref(:source)
+    end
+
+    with_them do
+      it { should eq compacted }
+    end
+  end
+
+  describe "Array#deep_compact_blank" do
+    subject { source.deep_compact_blank }
+
+    using DeepCompactor
+    using RSpec::Parameterized::TableSyntax
+
+    where(:source, :compacted) do
+      ["a", nil, [], ["aa", "bb", nil, []]] | ["a", %w[aa bb]]
+      ["a", "b", "c", %w[aa bb cc dd]]      | ref(:source)
     end
 
     with_them do
@@ -30,8 +46,24 @@ RSpec.describe DeepCompactor do
     using RSpec::Parameterized::TableSyntax
 
     where(:source, :compacted) do
-      { a: "1", b: nil, c: {}, d: { aa: "11", bb: "22", cc: nil } }   | { a: "1", c: {}, d: { aa: "11", bb: "22" } }
-      { a: "1", b: "2", c: {}, d: { aa: "11", bb: "22", cc: "33" } }  | ref(:source)
+      { a: "1", b: nil, c: {}, d: { aa: "11", bb: "22", cc: nil, dd: {} } }   | { a: "1", c: {}, d: { aa: "11", bb: "22", dd: {} } }
+      { a: "1", b: "2", c: {}, d: { aa: "11", bb: "22", cc: "33", dd: {} } }  | ref(:source)
+    end
+
+    with_them do
+      it { should eq compacted }
+    end
+  end
+
+  describe "Hash#deep_compact_blank" do
+    subject { source.deep_compact_blank }
+
+    using DeepCompactor
+    using RSpec::Parameterized::TableSyntax
+
+    where(:source, :compacted) do
+      { a: "1", b: nil, c: {}, d: { aa: "11", bb: "22", cc: nil, dd: {} } }                         | { a: "1", d: { aa: "11", bb: "22" } }
+      { a: "1", b: "2", c: { aa: "11" }, d: { aa: "11", bb: "22", cc: "33", dd: { aaa: "111" } } }  | ref(:source)
     end
 
     with_them do
