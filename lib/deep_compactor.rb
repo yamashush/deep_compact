3407ae927fcc834ba4d22b950b2e07a3633f58f0
yamashush <38120991+yamashush@users.noreply.github.com>
2024-01-06 17:36:25 +0900

    refactor

--- lib/deep_compactor.rb
+++ lib/deep_compactor.rb
@@ -5,6 +5,8 @@ require_relative "deep_compactor/version"
 # For deep compact nested Array and Hash.
 module DeepCompactor
   refine Array do
+    # @return [Array]
+    # @see https://docs.ruby-lang.org/en/master/Array.html#method-i-compact
     def deep_compact
       map do |value|
         if value.is_a?(Array) || value.is_a?(Hash)
@@ -15,16 +17,19 @@ module DeepCompactor
       end.compact
     end
 
+    # @return [self, nil]
+    # @see https://docs.ruby-lang.org/en/master/Array.html#method-i-compact
     def deep_compact!
-      map! do |value|
-        if value.is_a?(Array) || value.is_a?(Hash)
-          value.deep_compact
-        else
-          value
-        end
-      end.compact!
+      compacted = deep_compact
+
+      if self == compacted
+        nil
+      else
+        replace(compacted)
+      end
     end
 
+    # @return [Array]
     def deep_compact_blank
       map do |value|
         if value.is_a?(Array) || value.is_a?(Hash)
@@ -35,14 +40,15 @@ module DeepCompactor
       end.compact
     end
 
+    # @return [self, nil]
     def deep_compact_blank!
-      map! do |value|
-        if value.is_a?(Array) || value.is_a?(Hash)
-          value.empty? ? nil : value.deep_compact_blank
-        else
-          value
-        end
-      end.compact!
+      compacted = deep_compact_blank
+
+      if self == compacted
+        nil
+      else
+        replace(compacted)
+      end
     end
   end
 
