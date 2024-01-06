a669f1de886b1bd5246228309402d7689699020b
yamashush <38120991+yamashush@users.noreply.github.com>
2024-01-06 16:19:15 +0900

    Add deep_compact_blank

--- lib/deep_compactor.rb
+++ lib/deep_compactor.rb
@@ -2,12 +2,23 @@
 
 require_relative "deep_compactor/version"
 
+# For deep compact nested Array and Hash.
 module DeepCompactor
   refine Array do
     def deep_compact
       map do |value|
         if value.is_a?(Array) || value.is_a?(Hash)
           value.deep_compact
+        else
+          value
+        end
+      end.compact
+    end
+
+    def deep_compact_blank
+      map do |value|
+        if value.is_a?(Array) || value.is_a?(Hash)
+          value.empty? ? nil : value.deep_compact_blank
         else
           value
         end
@@ -20,6 +31,16 @@ module DeepCompactor
       to_h do |key, value|
         if value.is_a?(Array) || value.is_a?(Hash)
           [key, value.deep_compact]
+        else
+          [key, value]
+        end
+      end.compact
+    end
+
+    def deep_compact_blank
+      to_h do |key, value|
+        if value.is_a?(Array) || value.is_a?(Hash)
+          value.empty? ? [key, nil] : [key, value.deep_compact_blank]
         else
           [key, value]
         end
